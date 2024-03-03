eaaab7ca1af5974da9a5d15c227dc8b01a6851ba
Michailus <mihail.ab3@gmail.com>
2024-03-03 20:08:53 +0100

    Added settings activity with back button. Added button to settings in profile.

--- app/src/main/java/com/example/connectue/SettingsActivity.java
+++ app/src/main/java/com/example/connectue/SettingsActivity.java
@@ -5,22 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
-public class ProfileActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_settings);
 
-        NavigationBar.addNavigationBarActivitySwitch(this);
-
-        ImageView settings = findViewById(R.id.settings);
-        settings.setOnClickListener(new View.OnClickListener() {
+        View back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentProfile = new Intent(ProfileActivity.this, SettingsActivity.class);
+                Intent intentProfile = new Intent(SettingsActivity.this, ProfileActivity.class);
                 startActivity(intentProfile);
             }
         });
