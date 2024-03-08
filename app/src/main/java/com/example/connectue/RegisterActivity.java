b400f00d087a490f01cc7b2c227f5fb9a95b32b6
augustinlss <augustin.zhlass@gmail.com>
2024-03-08 17:10:27 +0100

    Fixed user verified checking.

--- app/src/main/java/com/example/connectue/RegisterActivity.java
+++ app/src/main/java/com/example/connectue/RegisterActivity.java
@@ -200,8 +200,6 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
     private void addUserToFirestore(String uid, String email, String password, String firstName, String lastName, String program) {
         User user = new StudentUser(uid, email, password, firstName, false, lastName, program);
         db.collection("users")
-                .document("UserTypes")
-                .collection("Students")
                 .document(uid)
                 .set(user)
                 .addOnCompleteListener(this,new OnCompleteListener<Void>() {
