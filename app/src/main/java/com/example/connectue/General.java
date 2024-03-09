e4492afe1e74aade6fddfc3cc30ffb6daa986149
Jie <jienl.tue@gmail.com>
2024-03-09 19:19:21 +0100

    1. Add phone to User 2. Add database parameter to General 3. profile page function fetch user data and show finish.

--- app/src/main/java/com/example/connectue/General.java
+++ app/src/main/java/com/example/connectue/General.java
@@ -1,9 +1,27 @@
 package com.example.connectue;
 
 public class General {
+
+//    user role parameters in database
     public static final int ADMIN = 0;
     public static final int STUDENT = 1;
     public static final int GUEST = 2;
+
+//    the name of user collection, and the field names within
+    public static final String USERCOLLECTION = "users";
+    public static final String EMAIL = "email";
+    public static final String FIRSTNAME = "firstName";
+    public static final String VERIFY = "isVerified";
+    public static final String LASTNAME = "lastName";
+    public static final String PASSWORD = "password";
+    public static final String POSTHISTORY = "postHistory";
+    public static final String PROFILEPICTURE = "profilePicURL";
+    public static final String PROGRAM = "program";
+    public static final String REVIEWHISTORY = "reviewHistory";
+    public static final String ROLE = "role";
+    public static final String COURSE = "userCourses";
+    public static final String USERID = "userId";
+    public static final String PHONE = "phone";
 
     public static boolean isAdmin(int role) {
         return role == ADMIN;
--- app/src/main/java/com/example/connectue/ProfileFragment.java
+++ app/src/main/java/com/example/connectue/ProfileFragment.java
@@ -1,14 +1,24 @@
 package com.example.connectue;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -27,16 +37,35 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private static final String TAG = "Profile";
 
     private FirebaseUser user;
     private FirebaseFirestore db;
     private DocumentSnapshot document;
-    private boolean isEditing = false;
-
+    private String firstNameStr = "";
+    private String lastNameStr = "";
+    private String emailStr = "";
+    private String phoneStr = "";
+    private String majorStr = "";
+    private boolean isEditing;
+    private boolean isAdmin;
+    Button editBtn;
+    Button logoutBtn;
+    Button deleteBtn;
+    Button postHisBtn;
+    Button reviewHisBtn;
+    Button adminBtn;
+    EditText firstName_fld;
+    EditText lastName_fld;
+    EditText major_fld;
+    EditText email_fld;
+    EditText phone_fld;
 
 
     public ProfileFragment() {
         // Required empty public constructor
+        isEditing = false;
+        isAdmin = false;
     }
 
     /**
@@ -64,12 +93,97 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        Log.i(TAG, "start executing onCreate");
+        db = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        Log.i(TAG, user.getUid());
+        DocumentReference documentReference = db.collection(General.USERCOLLECTION).document(user.getUid());
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    document = task.getResult();
+
+                    if (document.exists()) {
+                        Log.i(TAG,"document fetched");
+                        Log.i(TAG, document.toString());
+                        parseDocument();
+                        updateUIComponents();
+                    } else {
+                        Log.d(TAG, "No such user document");
+                    }
+                } else {
+                    Log.d(TAG, "get task failed with ", task.getException());
+                }
+            }
+        }
+        );
+        Log.i(TAG, "end executing onCreate, first name: " + firstNameStr);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        Log.i(TAG, "start executing crete view");
+        Log.i(TAG, "before: " + firstNameStr);
+        initComponents(view);
+        return view;
+    }
+
+    private void parseDocument() {
+        if (document.get(General.FIRSTNAME) != null) {
+            Log.i(TAG, "document-first name not null");
+            Log.i(TAG, "before: " + firstNameStr);
+            firstNameStr = (String) document.get(General.FIRSTNAME);
+            Log.i(TAG,"after: " + firstNameStr);
+        }
+        if (document.get(General.LASTNAME) != null) {  lastNameStr = (String) document.get(General.LASTNAME);}
+        if (document.get(General.EMAIL) != null) { emailStr = (String) document.get(General.EMAIL);}
+        if (document.get(General.PROGRAM) != null) { majorStr = (String) document.get(General.PROGRAM);}
+        if (document.get(General.PHONE) != null) { phoneStr = (String) document.get(General.PHONE);}
+    }
+
+    private void initComponents(View view) {
+        firstName_fld = view.findViewById(R.id.text_firstName);
+        lastName_fld = view.findViewById(R.id.text_lastName);
+        major_fld = view.findViewById(R.id.text_major);
+        email_fld = view.findViewById(R.id.text_email);
+        phone_fld = view.findViewById(R.id.text_phone);
+
+        editBtn= view.findViewById(R.id.btn_edit);
+        logoutBtn = view.findViewById(R.id.btn_logout);
+        deleteBtn = view.findViewById(R.id.btn_deleteAccount);
+        postHisBtn = view.findViewById(R.id.btn_postHistory);
+        reviewHisBtn  = view.findViewById(R.id.btn_reviewHistory);
+        adminBtn  = view.findViewById(R.id.btn_admmin);
+
+        initTextSection(view);
+    }
+
+    private void initTextSection(View view) {
+        setTest();
+
+        firstName_fld.setEnabled(false);
+        lastName_fld.setEnabled(false);
+        major_fld.setEnabled(false);
+        email_fld.setEnabled(false);
+        phone_fld.setEnabled(false);
+    }
+
+    private void setTest() {
+        firstName_fld.setText(firstNameStr);
+        lastName_fld.setText(lastNameStr);
+        major_fld.setText(majorStr);
+        email_fld.setText(emailStr);
+        phone_fld.setText(phoneStr);
+    }
+
+    private void updateUIComponents() {
+        if (getContext() != null) {
+            setTest();
+        }
     }
 }
