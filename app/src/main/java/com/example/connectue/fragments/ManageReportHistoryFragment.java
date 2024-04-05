db5b48e74005b602a1bd85c59851be14289d1dc0
Jie <jienl.tue@gmail.com>
2024-04-05 14:39:43 +0200

    1. Add more comments to ProfilePageSignoutDelete, ManageReportHistoryFragment, ReportItemCallback; 2. delete unused code in ProfileFragment

--- app/src/main/java/com/example/connectue/fragments/ManageReportHistoryFragment.java
+++ app/src/main/java/com/example/connectue/fragments/ManageReportHistoryFragment.java
@@ -50,6 +50,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * The workbench for administrator to see the reported content, and choose to delete or keep
+ *  the contents.
  * A simple {@link Fragment} subclass.
  * Use the {@link ManageReportHistoryFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -65,11 +67,8 @@ public class ManageReportHistoryFragment extends Fragment {
     private String mParam1;
     private String mParam2;
     private FirebaseFirestore db;
-//    TODO: once other button is clicked, remember to clear/reinit reportIds
     private List<QueryDocumentSnapshot> reports;
-//    TODO: ODO: once other button is clicked, remember to switch channel names
     private String currentChannel = General.POSTCOLLECTION;
-//    todo: set to null once it is deleted or released, fetchable false, picture not clickbale, delete keep to unclickable
     private DocumentSnapshot currentDocument = null;
     private DocumentReference currentContentReference = null;
     private DocumentReference currentRequestReference = null;
@@ -91,10 +90,7 @@ public class ManageReportHistoryFragment extends Fragment {
     private static final String TAG_load_content = "loadContent";
     private static final String TAG_Load_report = "reportLoad";
     private static final String TAG_delete = "delete";
-
-    /**
-     * Id of the content to delete.
-     */
+     //Id of the content to delete.
     String contentId;
 
     public ManageReportHistoryFragment() {
@@ -110,7 +106,6 @@ public class ManageReportHistoryFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ManageReportHistoryFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ManageReportHistoryFragment newInstance(String param1, String param2) {
         ManageReportHistoryFragment fragment = new ManageReportHistoryFragment();
         Bundle args = new Bundle();
@@ -164,7 +159,6 @@ public class ManageReportHistoryFragment extends Fragment {
         CollectionReference collectionReference = FirebaseFirestore.getInstance().collection(General.REPORTEDCOLLECTION);
         Query targetQuery = collectionReference.whereEqualTo(General.REPORTFROMCOLLECTION, fromCollection)
                 .orderBy(General.REPORTCOUNTER, Query.Direction.DESCENDING);
-
         targetQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -230,6 +224,10 @@ public class ManageReportHistoryFragment extends Fragment {
         });
     }
 
+    /**
+     * I
+     * @param view
+     */
     private void initComponents(View view) {
         db = FirebaseFirestore.getInstance();
         postChannelBtn = view.findViewById(R.id.report_load_post_btn);
@@ -245,7 +243,6 @@ public class ManageReportHistoryFragment extends Fragment {
         contentTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                contentTextZoomIn();
             }
         });
         contentInfoTV.setText(DEFAULT_COUNT);
@@ -269,16 +266,6 @@ public class ManageReportHistoryFragment extends Fragment {
             @Override
             public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
                 isImageFetchable = true;
-                contentIV.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(getContext(), "Can click",
-                            Toast.LENGTH_SHORT).show();
-                        contentImageZoomIn();
-                    }
-                });
-                Log.i(TAG_delete
-                        , "after fetch picture, : isImageFetchable: " + isImageFetchable);
                 return false;
             }
         }).into(contentIV);
@@ -307,7 +294,6 @@ public class ManageReportHistoryFragment extends Fragment {
         deleteBtn.setOnClickListener(null);
         keepBtn.setOnClickListener(null);
     }
-
 
     private void deleteContent() {
         Log.i(TAG_delete, "before delete picture, picture fetchable: " + isImageFetchable);
@@ -423,14 +409,6 @@ public class ManageReportHistoryFragment extends Fragment {
         loadReportedContents(currentChannel);
     }
 
-    private void contentImageZoomIn() {
-
-    }
-
-    private void contentTextZoomIn() {
-
-    }
-
     private void initPostButton() {
         postChannelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -438,7 +416,6 @@ public class ManageReportHistoryFragment extends Fragment {
                 currentChannel = General.POSTCOLLECTION;
                 resetAfterDeleteKeep();
                 loadReportedContents(currentChannel);
-
             }
         });
 
@@ -463,6 +440,4 @@ public class ManageReportHistoryFragment extends Fragment {
             }
         });
     }
-
-
 }
--- app/src/main/java/com/example/connectue/interfaces/ReportItemCallback.java
+++ app/src/main/java/com/example/connectue/interfaces/ReportItemCallback.java
@@ -2,6 +2,15 @@ package com.example.connectue.interfaces;
 
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+/**
+ * Each item in the to do list of the admin mode, once it is clicked, pass QueryDocumentSnapshot of
+ *  this item to the upper class to do the further operations.
+ */
 public interface ReportItemCallback {
+    /**
+     *  Let the other class to implement what to do next once this item is clicked.
+     * @param content QueryDocumentSnapshot storing the information of this item.
+     *                Note, this content is the document storing the report information.
+     */
     public void itemClicked(QueryDocumentSnapshot content);
 }
